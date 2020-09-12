8bf224b629e3fa4091a96f725cf39e8e360defda
anibal0126 <amunozg_1@uqvirtual.edu.co>
2020-09-12 12:08:42 -0500

    Nuevo

---
+++ src/java/modelo/Libro.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+public class Libro {
+
+	private Integer idLibro;
+	private String nombre;
+	private String autor;
+	private String estado;
+	private String observacion;
+
+	public Libro() {
+	}
+
+	public Libro(Integer idLibro, String nombre, String autor, String estado,
+			String observacion) {
+		this.idLibro = idLibro;
+		this.nombre = nombre;
+		this.autor = autor;
+		this.estado = estado;
+		this.observacion = observacion;
+	}
+
+	public Integer getIdLibro() {
+		return idLibro;
+	}
+
+	public void setIdLibro(Integer idLibro) {
+		this.idLibro = idLibro;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getObservacion() {
+		return observacion;
+	}
+
+	public void setObservacion(String observacion) {
+		this.observacion = observacion;
+	}
+
+}
---
+++ src/java/modelo/LibroBean.java
@@ -0,0 +1,69 @@
+package modelo;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean  
+@RequestScoped
+public class LibroBean implements Serializable {
+
+	private static final long serialVersionUID = 6081417964063918994L;
+        
+
+	public List<Libro> getLibros() throws ClassNotFoundException, SQLException {
+            
+		Connection connect = null;
+
+		String url = "jdbc:mysql://localhost:3306/libros";
+
+		String username = "root";
+		String password = "0711";
+
+		try {
+
+			Class.forName("com.mysql.jdbc.Driver");
+
+			connect = DriverManager.getConnection(url, username, password);
+			System.out.println("Connection established"+connect);
+
+		} catch (SQLException ex) {
+			System.out.println("in exec");
+			System.out.println(ex.getMessage());
+		}
+
+		List<Libro> libros = new ArrayList<>();
+		PreparedStatement pstmt = connect.prepareStatement("select idLibro, nombre, autor, estado, observacion from Libros");
+		ResultSet rs = pstmt.executeQuery();
+
+		while (rs.next()) {
+                    
+                   
+			Libro libro = new Libro();
+			libro.setIdLibro(rs.getInt("idLibro"));
+			libro.setNombre(rs.getString("nombre"));
+			libro.setAutor(rs.getString("autor"));
+			libro.setEstado(rs.getString("estado"));
+			libro.setObservacion(rs.getString("observacion"));
+
+			libros.add(libro);
+
+		}
+
+		// close resources
+		rs.close();
+		pstmt.close();
+		connect.close();
+
+		return libros;
+
+	}
+
+}
